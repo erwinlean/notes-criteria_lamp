2d41bdaf43721e8b8eb71ae8b59b1ea8b3d6c322
Erwin Marte <85772547+erwinlean@users.noreply.github.com>
2022-11-17 10:08:51 -0300

    Update Ubuntu-Akeneo_Community_6_Docker_.sh

--- notes/Ubuntu-Akeneo_Community_6_Docker_.sh
+++ notes/Ubuntu-Akeneo_Community_6_Docker_.sh
@@ -17,6 +17,7 @@
 	sudo apt-get update
 	sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
+	sudo docker-compose version
 
 # Instalacion akeneo PIM
 	mkdir pim
