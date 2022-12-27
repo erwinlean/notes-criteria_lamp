8b0f34569bde5a03711b2f442a455734c9182825
Erwin Marte <85772547+erwinlean@users.noreply.github.com>
2022-12-27 11:09:06 -0300

    Update akeneo-minors_upgrades.sh

--- notes/akeneo-minors_upgrades.sh
+++ notes/akeneo-minors_upgrades.sh
@@ -10,7 +10,7 @@ rm -rf var/cache/* ./public/bundles/* ./public/css/* ./public/js/*
 rm yarn.lock # May not be necessary
 sudo bin/console pim:installer:assets
 bin/console cache:warmup
-yarn install
+yarn install # sudo bin/console pim:installer:dumb-require-path
 yarn run less
 yarn run webpack
 # In case of error with "yarn run webpack":
